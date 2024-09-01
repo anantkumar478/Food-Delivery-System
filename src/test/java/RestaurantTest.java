fba350441ce5fd6da5d9bea41a12d09ea6ab90b5
Anant Kumar Mathur <anantkumar_mathur@epam.com>
2024-09-01 11:46:58 +0530

    Updated Tests

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -8,14 +8,14 @@ public class RestaurantTest {
     @Test
     public void testAddDish() {
         Restaurant restaurant = new Restaurant("Pasta Palace");
-        Dish dish = DishFactory.createDish("Italian", "Spaghetti", 12.0);
+        Dish dish = DishFactory.createDish(DishFactory.DishType.valueOf("Italian"), "Spaghetti", 12.0);
         restaurant.addDish(dish);
         assertEquals("Menu size should be 1 after adding one dish", 1, restaurant.getMenu().size());
     }
 
     @Test
     public void testDishDetails() {
-        Dish dish = DishFactory.createDish("Chinese", "Dumplings", 10.0);
+        Dish dish = DishFactory.createDish(DishFactory.DishType.valueOf("Chinese"), "Dumplings", 10.0);
         assertEquals("Dumplings", dish.getName());
         assertEquals(10.0, dish.getPrice(), 0.001);
     }
