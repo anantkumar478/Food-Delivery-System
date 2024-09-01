5a469764fc241c7498a2df15542c7b22df6f1bc7
Anant Kumar Mathur <anantkumar_mathur@epam.com>
2024-09-01 11:46:19 +0530

    Updated DishFactory

--- src/main/java/org/restaurant/DishFactory.java
+++ src/main/java/org/restaurant/DishFactory.java
@@ -1,16 +1,26 @@
 package org.restaurant;
 
 public class DishFactory {
-    public static Dish createDish(String type, String name, double price) {
-        if ("Italian".equalsIgnoreCase(type)) {
-            return new ItalianDish(name, price);
-        } else if ("Chinese".equalsIgnoreCase(type)) {
-            return new ChineseDish(name, price);
-        } else if ("American".equalsIgnoreCase(type)){
-            return new AmericanDish(name, price);
-        } else {
-            return null;
+
+    public enum DishType {
+        Italian,
+        Chinese,
+        American
+    }
+
+    public static Dish createDish(DishType type, String name, double price) throws IllegalArgumentException {
+        switch (type) {
+            case Italian:
+                return new ItalianDish(name, price);
+
+            case Chinese:
+                return new ChineseDish(name, price);
+
+            case American:
+                return new AmericanDish(name, price);
+
+            default:
+                throw new IllegalArgumentException("Invalid Dish Type");
         }
     }
 }
-
